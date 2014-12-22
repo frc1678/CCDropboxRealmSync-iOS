e9b8e5c1391c4195b314a70bd718264e2099200f
Donald Pinckney <donald_pinckney@icloud.com>
2014-12-22 13:48:44 -0800

    Added new class files

---
+++ Classes/CCRealmSync.h
@@ -0,0 +1,26 @@
+//
+//  CCRealmSync.h
+//  Pods
+//
+//  Created by Donald Pinckney on 12/22/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+//#import "Realm.h"
+
+//#import <Realm/RLMArray.h>
+//#import <Realm/RLMMigration.h>
+//#import <Realm/RLMObject.h>
+//#import <Realm/RLMPlatform.h>
+//#import <Realm/RLMRealm.h>
+//#import <Realm/RLMResults.h>
+//#import <Realm/RLMSchema.h>
+//#import <Realm/RLMSwiftHelpers.h>
+
+
+@interface CCRealmSync : NSObject
+
++ (RLMRealm *)defaultDBRealm;
+
+@end
---
+++ Classes/CCRealmSync.m
@@ -0,0 +1,13 @@
+//
+//  CCRealmSync.m
+//  Pods
+//
+//  Created by Donald Pinckney on 12/22/14.
+//
+//
+
+#import "CCRealmSync.h"
+
+@implementation CCRealmSync
+
+@end
